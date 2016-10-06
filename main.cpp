d84d1c2603cf4b880cabccbd947d389e1b01b3bb
Mick McCarthy <mccarthy.mick0012@gmail.com>
2016-10-06 23:30:01 +0100

    Add game into and pseudocode for game layout

--- main.cpp
+++ main.cpp
@@ -1,3 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
 int main () {
+	// Introduce the game
+	constexpr int WORD_LENGTH = 5;
+	cout << "Welcome to Bulls and Cows\n";
+	cout << "Can you guess the " << WORD_LENGTH << " letter isogram I'm thinking of?\n";
+	
+	// Get the guess from the player
+
+	// Repeat the guess back to them
+	
 	return 0;
 }
