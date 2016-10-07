0656fe251cf1ddef452786e5b23924e82c6175e3
Mick McCarthy <mccarthy.mick0012@gmail.com>
2016-10-07 14:26:38 +0100

    Refactor code into seperate methods

--- main.cpp
+++ main.cpp
@@ -3,19 +3,35 @@
 
 using namespace std;
 
+
+void PrintIntro();
+void PlayGame();
+string GetGuess();
+
 int main () {
-	// Introduce the game
+	PrintIntro();
+	PlayGame();
+	return 0;
+}
+
+void PrintIntro() {
 	constexpr int WORD_LENGTH = 5;
 	cout << "Welcome to Bulls and Cows\n";
 	cout << "Can you guess the " << WORD_LENGTH << " letter isogram I'm thinking of?\n";
-	
-	// Get the guess from the player
+	return;
+}
+
+void PlayGame() {
+	constexpr int NUM_GUESSES = 5;
+	for (int i = 0; i < NUM_GUESSES; i++) {
+		cout << "Your guess was " << GetGuess() << ".\n";
+		cout << endl;
+	}
+}
+
+string GetGuess() {
 	cout << "Enter your guess: ";
 	string Guess = "";
 	getline(cin, Guess);
-
-	// Repeat the guess back to them
-	cout << "Your guess was " << Guess << ". Is that right?\n";
-	
-	return 0;
+	return Guess;
 }
